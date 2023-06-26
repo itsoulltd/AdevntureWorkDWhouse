78909e198a03bcc01633cbc27bb1d2e844af73ff
Islam <towhid.islam@degruyter.com>
2023-06-26 21:49:17 +0200

    updated

--- src/database/Exercise_51_75.sql
+++ src/database/Exercise_51_75.sql
@@ -1,3 +1,43 @@
 
 -- Connect to AdventureWorks2022 Database
 USE AdventureWorks2022;
+
+/*
+-- 51
+*/
+SELECT TOP 10
+	*
+FROM A_Table
+ORDER BY A_Column;
+
+/*
+-- 52
+*/
+SELECT TOP 10
+	*
+FROM A_Table
+ORDER BY A_Column;
+
+/*
+-- 53
+*/
+SELECT TOP 10
+	*
+FROM A_Table
+ORDER BY A_Column;
+
+/*
+-- 54
+*/
+SELECT TOP 10
+	*
+FROM A_Table
+ORDER BY A_Column;
+
+/*
+-- 55
+*/
+SELECT TOP 10
+	*
+FROM A_Table
+ORDER BY A_Column;
--- src/database/Exercise_76_100.sql
+++ src/database/Exercise_76_100.sql
@@ -1,3 +1,43 @@
 
 -- Connect to AdventureWorks2022 Database
 USE AdventureWorks2022;
+
+/*
+-- 76
+*/
+SELECT TOP 10
+	*
+FROM A_Table
+ORDER BY A_Column;
+
+/*
+-- 77
+*/
+SELECT TOP 10
+	*
+FROM A_Table
+ORDER BY A_Column;
+
+/*
+-- 78
+*/
+SELECT TOP 10
+	*
+FROM A_Table
+ORDER BY A_Column;
+
+/*
+-- 79
+*/
+SELECT TOP 10
+	*
+FROM A_Table
+ORDER BY A_Column;
+
+/*
+-- 80
+*/
+SELECT TOP 10
+	*
+FROM A_Table
+ORDER BY A_Column;
